1b72b5d7ed1c1b37c8c80ecf426289b698f6b5e0
novicaa <novicamiketic@gmail.com>
2015-04-06 11:19:36 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa=" 1265 Airport Rd South, Naples, Florida 34104"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Florida 34104";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2396592996;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="fade"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
