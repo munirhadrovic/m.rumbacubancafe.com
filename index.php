d15ebff050ab199b9e59d01012e4eb593366d8d8
novicaa <novicamiketic@gmail.com>
2015-04-14 14:51:40 +0200

    promjene footer

--- index.php
+++ index.php
@@ -100,7 +100,7 @@
 		    <h1><a href="#page1"><img src="img/logo.png" alt="LOGO"></a></h1>
 		</div>
 		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
-			<h1>Powered by <a href="http://mobilegrows.com/">MobileGrows</a> | <a href="http://www.rumbacubancafe.com/" rel="external">Desktop Site</a></h1>
+			<h1>Powered by <a href="http://mobilegrows.com/" rel="external">MobileGrows</a> | <a href="http://www.rumbacubancafe.com/" rel="external">Desktop Site</a></h1>
 		</div>
 		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
 		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
